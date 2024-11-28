f733bca868576836b37c1427fbb49ef1d4f1b98f
Antoniel <antonielzeferino0@gmail.com>
2024-11-28 15:38:53 -0300

    project cards update

--- src/components/projectsList.jsx
+++ src/components/projectsList.jsx
@@ -1,4 +1,5 @@
 import { useIdiom } from "@/provider/idiomProvider";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const cardStyle = "relative w-full h-auto max-w-[400px] max-h-[400px] border-blue-600 border-2 rounded-xl overflow-hidden group";
@@ -6,6 +7,7 @@ const cardStyle = "relative w-full h-auto max-w-[400px] max-h-[400px] border-blu
 function ProjectsList() {
   const { idiom } = useIdiom();
   const [t, setTranslation] = useState({});
+  const [loadedImages, setLoadedImages] = useState({});
 
   useEffect(() => {
     const loadTranslations = async () => {
@@ -13,26 +15,40 @@ function ProjectsList() {
       const data = await res.json();
       setTranslation(data);
     };
-    loadTranslations()
-  },[idiom])
+    loadTranslations();
+  }, [idiom]);
+
+  const handleImageLoad = (index) => {
+    setLoadedImages((prev) => ({ ...prev, [index]: true }));
+  };
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 place-items-center w-full min-h-[500px] p-4">
-      {t.projects && t.projects.map((project, index) => (
-        <div key={index} className={cardStyle}>
-          <video
-            src={project.src}
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover rounded-xl"
-          ></video>
-          <div className="absolute bottom-0 left-0 w-full h-1/4 bg-gradient-to-t from-black via-transparent/90 to-transparent/40 opacity-100 group-hover:h-full group-hover:opacity-100 transition-all duration-300 rounded-b-xl flex flex-wrap items-end justify-center p-2">
-            <p className="text-white font-bold text-md md:text-xl text-center p-2">{project.title}</p>
-            <p className="text-justify">{project.description}</p>
+      {t.projects &&
+        t.projects.map((project, index) => (
+          <div
+            key={index}
+            className={`${cardStyle} ${
+              loadedImages[index] ? "opacity-100" : "opacity-0"
+            } transition-opacity duration-500`}
+          >
+            <Image
+              src={project.src}
+              alt={project.title}
+              width={600}
+              height={600}
+              onLoadingComplete={() => handleImageLoad(index)}
+            />
+            <div className="absolute bottom-0 left-0 w-full h-1/5 bg-gradient-to-t from-black via-transparent/90 to-transparent/40 opacity-0 group-hover:h-full group-hover:opacity-100 transition-all duration-700 rounded-b-xl flex flex-wrap items-end justify-center p-2">
+              <p className="text-white font-bold text-md md:text-xl text-center p-2">
+                {project.title}
+              </p>
+              <p className="indent-4 text-justify px-2 project-description">
+                {project.description}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
