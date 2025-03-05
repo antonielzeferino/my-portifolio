cd8637e0b6f0db3f5143ca922067185bc5dcc11c
Antoniel Zeferino <114781545+antonielzeferino@users.noreply.github.com>
2025-03-05 20:04:08 -0300

    Update projectsList.jsx

--- src/components/projectsList.jsx
+++ src/components/projectsList.jsx
@@ -42,7 +42,7 @@ function ProjectsList() {
             <div className="absolute bottom-0 left-0 w-full h-1/5 bg-gradient-to-t from-black via-transparent/90 to-transparent/40 opacity-0 group-hover:h-full group-hover:opacity-100 transition-all duration-700 rounded-b-xl flex flex-wrap items-end justify-center p-2">
             <Link href={project.link} target="_blank">
 
-              <h4 className="text-white font-bold text-md md:text-xl text-center p-2">
+              <h4 className="text-white font-bold text-md md:text-xl text-center p-2 underline">
                 {project.title}
               </h4>
               <p className="indent-4 text-justify px-2 project-description">
